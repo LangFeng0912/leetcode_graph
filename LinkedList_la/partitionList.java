1fdd32eab778faac856c1776ba88a1b2ff94de53
fenglang <Feng3133504033@outlook.com>
2023-02-27 09:04:39 +0100

    86. 分隔链表（记得断开原链表的next节点）

--- LinkedList_la/partitionList.java
+++ LinkedList_la/partitionList.java
@@ -31,4 +31,28 @@ public class partitionList {
         // System.out.println("yes");
         return dummy.next;
     }
+
+    // 合并链表的方法，但是要记得断开原序列的next
+    public ListNode partition1(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0);
+        ListNode dummy2 = new ListNode(0);
+        ListNode cur = head;
+        ListNode l1 = dummy1;
+        ListNode l2 = dummy2;
+        while(cur != null){
+            if(cur.val<x){
+                l1.next = cur;
+                l1 = l1.next;
+            }else{
+                l2.next = cur;
+                l2 = l2.next;
+            }
+            ListNode temp = cur.next;
+            cur.next = null;
+            cur = temp;
+        }
+        l1.next = dummy2.next;
+        return dummy1.next;
+    }
+
 }
